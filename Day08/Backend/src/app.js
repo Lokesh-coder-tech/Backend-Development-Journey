4e6c66b8e43347c08c61b92a5ef9a13dc3a70751
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-01-31 23:05:48 +0530

    Day08

---
+++ Day08/Backend/src/app.js
@@ -0,0 +1,53 @@
+const express = require("express")
+const noteModel = require("./models/notemodel")
+const app = express();
+const cors = require("cors")
+
+app.use(cors())
+app.use(express.json())
+
+app.post("/notes", async (req, res) => {
+    const {title, description} = req.body
+
+    const note = await noteModel.create({
+        title, description
+    })
+    res.status(201).json({
+        message: "note created successfully",
+        note
+    })
+})
+
+app.get("/notes", async (req, res) => {
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        message: "note fetched successfully",
+        notes
+    })
+})
+
+app.delete("/notes/:id", async (req, res) => {
+   const id = req.params.id
+
+   await noteModel.findByIdAndDelete(id)
+
+   res.status(204).json({
+     message: "note deleted successfully"
+   })
+
+})
+
+app.patch("/notes/:id", async (req, res) => {
+    const id = req.params.id
+    const {description} = req.body
+
+    await noteModel.findByIdAndUpdate(id, {description})
+
+    res.status(200).json({
+        message: "note updated successfully"
+    })
+
+})
+
+module.exports = app
