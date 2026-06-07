ae529b9316bb9846863f96777aeeb70e919ef55d
Lokesh Sharma <iamlokesh1406@gmail.com>
2026-06-07 16:48:27 +0530

    feat: add initial HTML file and configure CORS for multiple origins

--- Nexora/Backend/src/app.js
+++ Nexora/Backend/src/app.js
@@ -1,3 +1,7 @@
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import express from "express";
 import authRouter from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
@@ -12,14 +16,40 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser())
 app.use(morgan("dev"))
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: [ "GET", "POST", "PUT", "DELETE" ],
-}))
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+//   "https://snitch-9ajg.onrender.com",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "DELETE", "PATCH"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use("/api/auth", authRouter);
 app.use("/api/chats", chatRouter)
+
+
+// Static file serving
+app.use(express.static('public'));
+
+// SPA fallback middleware - MUST be last to catch all unmatched routes
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 
 export default app;
